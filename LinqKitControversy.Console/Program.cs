f044b0840e22231684de6cb9dd77a570f6ebc554
Leonid Tsarev <leo@bastilia.ru>
2019-10-04 19:11:18 +0300

    initial commit

---
+++ LinqKitControversy.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+using LinqKit;
+
+namespace LinqKitControversy.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var ef6Ctx = new Ef6Context();
+            var foo = await ef6Ctx.FooSet.AsExpandable().ToListAsync();
+
+            var coreCtx = new CoreContext();
+            var bar = await coreCtx.BarSet.AsExpandable().ToListAsync();
+
+        }
+    }
+}
